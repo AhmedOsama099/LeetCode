00f6f7dea3349b578b7e43a362c6f2aa06f05a38
AhmedOsama099 <74177446+AhmedOsama099@users.noreply.github.com>
2024-04-28 01:30:30 +0300

    Update 0006-zigzag-conversion.js

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -3,6 +3,20 @@
  * @param {number} numRows
  * @return {string}
  */
+
+/*
+idea: 
+Looping on array with something like clock panadol, when its hits 0 it increase
+and when it hits numRows - 1 it decreases
+
+another point for strings intialization inside array
+we used this condition 
+   if (i - numRows < 0) {
+      resultArr.push("");
+    }
+    
+*/
+
 var convert = function (s, numRows) {
   if (numRows === 1) {
     return s;
