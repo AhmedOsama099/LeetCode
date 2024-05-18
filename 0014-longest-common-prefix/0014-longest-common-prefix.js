8b4f822534583030f57e8e79f7b43475f55d4166
AhmedOsama099 <74177446+AhmedOsama099@users.noreply.github.com>
2024-05-19 02:18:11 +0300

    Time: 58 ms (45.90%), Space: 51 MB (18.11%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+var checkCommon = function (str1, str2) {
+    let res = ""
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] === str2[i]) {
+            res = res + str1[i]
+        } else {
+            return res
+        }
+    }
+    return res
+}
+
+var longestCommonPrefix = function (strs) {
+    if (strs.length <= 1) return strs[0]
+
+    let commonPerfix = checkCommon(strs[0], strs[1])
+
+    for (let i = 2; i < strs.length; i++) {
+        commonPerfix = checkCommon(commonPerfix, strs[i])
+    }
+    return commonPerfix
+};
+
