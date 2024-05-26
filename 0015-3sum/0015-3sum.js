0900a676184f51e6b71acd7d1eeba90eeea50f8e
AhmedOsama099 <74177446+AhmedOsama099@users.noreply.github.com>
2024-05-26 20:57:16 +0300

    Time: 178 ms (31.17%), Space: 65.7 MB (40.38%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    nums.sort((a, b) => a - b);
+    let result = [];
+    for (let i = 0; nums[i] <= 0; i++) {
+        if (nums[i] === nums[i - 1]) continue;
+        let right = i + 1, left = nums.length - 1;
+        while (right < left) {
+            let res = nums[i] + nums[right] + nums[left]
+            if (!res) {
+                let stringArr = JSON.stringify([nums[i], nums[right], nums[left]].sort());
+                result.push([nums[i], nums[right], nums[left]]);
+                left--
+                right++
+                while(right < left && nums[left] === nums[left+1]) left--
+                while(right < left && nums[right] === nums[right-1]) right++
+            }
+            else if (res > 0) {
+                left--
+            } else {
+                right++
+            }
+        }
+    }
+    return result;
+};
