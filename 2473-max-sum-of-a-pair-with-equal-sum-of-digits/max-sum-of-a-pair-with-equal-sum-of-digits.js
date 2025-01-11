aa30121af20c9f0e509e83210fbe6fda92d98f68
Ahmed Osama <74177446+AhmedOsama099@users.noreply.github.com>
2025-01-11 20:26:36 +0200

    Time: 25 ms (100.00%) | Memory: 61.8 MB (93.51%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -2,14 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+ function getSumOfDigits(num) {
+  num = Math.abs(num); // Handle negative numbers
+  let sum = 0;
+
+  while (num > 0) {
+    sum += num % 10; // Get the last digit
+    num = Math.floor(num / 10); // Remove the last digit
+  }
+
+  return sum;
+}
+
 var maximumSum = function (nums) {
 
     let cash = {}
     let max = -1;
     for (let i = 0; i < nums.length; i++) {
-        let num = nums[i].toString().split("").reduce((acc, curr) => {
-            return Number(acc) + Number(curr)
-        }, 0)
+        let num = getSumOfDigits(nums[i])
         if (cash[num]) {
             max = Math.max(max, nums[i] + cash[num])
             cash[num] = Math.max(nums[i], cash[num])
