76ab8f89c4956b9b7ce3ad63e463ee0bd53884d6
Ahmed Osama <74177446+AhmedOsama099@users.noreply.github.com>
2025-01-19 11:08:37 +0200

    Time: 1 ms (76.76%) | Memory: 49.5 MB (47.57%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function (arr) {
+    if (arr.length < 1) return false
+    if (arr.length === 2) return true
+
+    arr.sort((a, b) => a - b);
+
+    let value = arr[1] - arr[0]
+    for (let i = 1; i < arr.length - 1; i++) {
+        val = arr[i + 1] - arr[i]
+        if (val !== value) {
+            return false
+        }
+    }
+
+    return true
+};
