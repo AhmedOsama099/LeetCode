06d89f28d1f901a2d95ffa78fea211056682638c
Ahmed Osama <74177446+AhmedOsama099@users.noreply.github.com>
2025-01-20 20:41:41 +0200

    Time: 72 ms (9.43%) | Memory: 51.3 MB (31.13%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var possibleStringCount = function(word) {
-    
     let counter  = 1;
-
     for(let i =0; i< word.length; i++){
         if(word[i] === word[i+1]){
             counter++
         }
     }
-
-    return counter
+    return counter;
 };
