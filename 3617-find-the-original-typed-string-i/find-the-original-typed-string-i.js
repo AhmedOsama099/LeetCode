e33a854a71faa4edf015f98fa98270e5b1c9476d
Ahmed Osama <74177446+AhmedOsama099@users.noreply.github.com>
2025-01-21 08:07:02 +0200

    Time: 79 ms (5.66%) | Memory: 51.5 MB (24.53%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -2,12 +2,32 @@
  * @param {string} word
  * @return {number}
  */
-var possibleStringCount = function(word) {
-    let counter  = 1;
-    for(let i =0; i< word.length; i++){
-        if(word[i] === word[i+1]){
+var possibleStringCount = function (word) {
+
+
+    let counter = 1;
+    let i = 0;
+    let j = word.length - 1
+    let isEven = word.length % 2 === 0
+    while (i < j) {
+        if (word[i] === word[i + 1]) {
             counter++
         }
+        if (word[j] === word[j - 1]) {
+            counter++
+        }
+        i++
+        j--
     }
-    return counter;
+
+
+    let half = word.length / 2
+
+    if (isEven && word[half - 1] === word[half] && counter > 1) {
+        console.log("here")
+        counter--
+    }
+
+    return counter
 };
+
