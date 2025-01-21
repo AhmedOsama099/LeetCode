4c2e634543fd8f66b7d2af399c988b0a5d4a764a
Ahmed Osama <74177446+AhmedOsama099@users.noreply.github.com>
2025-01-21 08:10:36 +0200

    Time: 57 ms (79.25%) | Memory: 50.8 MB (42.45%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -3,14 +3,40 @@
  * @return {number}
  */
 var possibleStringCount = function (word) {
- 
+
     let counter = 1;
-    for (let i = 0; i < word.length - 1; i++) {
+    let i = 0;
+    let j = word.length - 1
+    let isEven = word.length % 2 === 0
+    let half = word.length / 2
+    while (i < j) {
         if (word[i] === word[i + 1]) {
             counter++
         }
+        if (word[j] === word[j - 1]) {
+            counter++
+        }
+        i++
+        j--
+    }
+
+    if (isEven && word[half - 1] === word[half] && counter > 1) {
+        counter--
     }
     return counter;
+
+    /* - Another Solution
+     
+        let counter = 1;
+        for (let i = 0; i < word.length - 1; i++) {
+            if (word[i] === word[i + 1]) {
+                counter++
+            }
+        }
+        return counter; 
+    */
+
+
 
 
 };
